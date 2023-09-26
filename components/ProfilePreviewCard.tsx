d70a86273c7d94485606bf3dfc3f24413c817d58
Jack Lin <hellojjlin@gmail.com>
2023-09-25 23:22:03 -0400

    changed the home page to fetch data server side

--- components/ProfilePreviewCard.tsx
+++ components/ProfilePreviewCard.tsx
@@ -1,5 +1,5 @@
-import { Card, Spinner } from "react-bootstrap";
-import styles from "@/styles/app.module.css";
+import { Card, ListGroup, Spinner } from "react-bootstrap";
+import styles from "@/styles/profilePreviewCard.module.css";
 import { GetServerSideProps } from "next";
 import { DailyOpenClose } from "@/models/DailyOpenClose";
 import { useState, useEffect } from "react";
@@ -7,62 +7,22 @@ import { useState, useEffect } from "react";
 interface ProfilePreviewCardProps {
   name: string,
   imageURL: string,
+  stockData: DailyOpenClose,
 }
 
-const ProfilePreviewCard = ( {name, imageURL} : ProfilePreviewCardProps ) => {
-
-  // state variables
-  const [ data, setData ] = useState<DailyOpenClose | null>(null);
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ dataError, setDataError ] = useState(false);
-
-  // async function that retrieves data
-  async function getData( name: string ) {
-    try {
-        setData(null);
-        setIsLoading(true);
-        setDataError(false);
-        const response = await fetch("/api/search-prices?q=" + name);
-        const stockData: DailyOpenClose = await response.json();
-        if (stockData.error) { // handles specific error responses from the API
-          setDataError(true);
-        } else {
-          setData(stockData);
-          console.log(stockData);
-        }
-    } catch (error) { // handles other types of errors such as network errors or JSON parsing errors
-        console.log(error);
-        setDataError(true);
-    } finally {
-        setIsLoading(false);
-    }
-  }
-  useEffect(() => {
-    getData(name);
-  }, [name]);
-
+// Below is not working for some reason.
+const ProfilePreviewCard = ( {name, imageURL, stockData} : ProfilePreviewCardProps ) => {
+  console.log("here is the object: " + stockData);
   return (
-    <Card className="h-100" bg="light" text="muted">
+    <Card className="h-100" bg="light" text="muted" style={{ width: '18rem' }}>
       <Card.Img src={imageURL} variant="top" className="img-fluid" />
       <Card.Body>
         <Card.Title className="display-5">{ name }</Card.Title>
-        <Card.Text className="mt-3">
-          { isLoading && <Spinner animation="border" /> }
-          { dataError && <p>Something went wrong with the API.</p> }
-          { data && !dataError && (
-            <>
-              <div className="d-flex justify-content-between">
-                <p>Open</p>
-                <p className="fw-bold">${data.open}</p>
-              </div>
-              <div className="d-flex justify-content-between">
-                <p>Close</p>
-                <p className="fw-bold">${data.close}</p>
-              </div>
-            </>            
-          )}
-        </Card.Text>
       </Card.Body>
+      <ListGroup variant="flush">
+        <ListGroup.Item className={`lead ${styles.listItemStyle}`}>Open: <strong>${ (stockData.open)?.toFixed(2) }</strong></ListGroup.Item>
+        <ListGroup.Item className={`lead ${styles.listItemStyle}`}>Close: <strong>${ stockData.close?.toFixed(2) }</strong></ListGroup.Item>
+      </ListGroup>
     </Card>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,18 @@ import { Inter } from 'next/font/google'
 import { Alert, Button, Col, Container, Form, Row } from 'react-bootstrap';
 import ProfilePreviewCard from '@/components/ProfilePreviewCard';
 import { CompanyNameAndURL } from '@/models/CompanyNameAndURL';
+import { DailyOpenClose } from '@/models/DailyOpenClose';
+import { GetServerSideProps } from 'next';
+
+interface companyPreviewData {
+  name: string,
+  imageURL: string,
+  stockData: DailyOpenClose,
+}
+
+interface HomeProps {
+  companyPreviewDataArray: companyPreviewData[],
+}
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,9 +24,28 @@ const companies: CompanyNameAndURL[] = [
   { name: 'GOOGL', URL: 'https://s3-symbol-logo.tradingview.com/alphabet--600.png'},
   { name: 'META', URL: 'https://static.vecteezy.com/system/resources/previews/022/100/699/original/meta-logo-transparent-free-png.png'},
   { name: 'IBM', URL: 'https://www.ibm.com/brand/experience-guides/developer/b1db1ae501d522a1a4b49613fe07c9f1/01_8-bar-positive.svg'},
+  { name: 'TSLA', URL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Tesla_logo.png/640px-Tesla_logo.png'},
+  { name: 'BABA', URL: 'https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/alibaba-512.png'},
+  { name: 'NVDA', URL: 'https://cdn-icons-png.flaticon.com/512/732/732230.png'},
 ];
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  let companyPreviewDataArray: companyPreviewData[] = [];
+  for (const company of companies) {
+    const response = await fetch(`https://api.polygon.io/v1/open-close/${company.name}/2023-01-09?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`);
+    const dataResponse = await response.json();
+    companyPreviewDataArray.push({
+      name: company.name,
+      imageURL: company.URL,
+      stockData: dataResponse,
+    });
+  }
+  return {
+    props: { companyPreviewDataArray }
+  }
+}
+
+export default function Home({companyPreviewDataArray}: HomeProps) {
   return (
     <>
       <Head>
@@ -26,10 +57,12 @@ export default function Home() {
           <h2 className="lead text-center">Simple, Efficient, Accurate</h2>
         </div>
         <Alert className="text-center">
-          At the moment, this page uses <strong>client-side data fetching</strong> to fetch fresh data.
-          It will soon be converted to <strong>server-side data fetching</strong>.
+          This page uses <strong>getServerSideProps</strong> to fetch data server side, which <strong>improves user experience and SEO.</strong>
         </Alert>
-        <Alert className="text-center">The <strong>Polygon API</strong> free subscription only allows for <strong>5 API Calls / Minute</strong>. Due to the API limitations, the data might not load.</Alert>
+        <Alert className="text-center">
+          This page runs on <strong><a href="https://polygon.io/">polygon.io</a></strong>.
+          
+        </Alert>
         <Form className="py-3">
           <Form.Group className="mb-3 d-flex flex-column align-items-center">
             <Form.Label className="display-4">Search Individual Stocks</Form.Label>
@@ -39,12 +72,12 @@ export default function Home() {
             <Button type="submit">Search</Button>
           </div>
         </Form>
-        <h2 className="display-5 text-center my-4">Popular Tech Companies</h2>
+        <h2 className="display-5 text-center my-4">Top Tech Companies</h2>
         <div className="pb-5 justify-content-center">
           <Row xs={1} sm={2} xl={3} className="g-3">
-            {companies.map((company) => (
+            {companyPreviewDataArray.map((company) => (
               <Col xl={3} lg={3} md={6} xs={10} key={company.name} className="mx-auto">
-                <ProfilePreviewCard name={company.name} imageURL={company.URL} />
+                <ProfilePreviewCard name={company.name} imageURL={company.imageURL} stockData={company.stockData} />
               </Col>
             ))}
           </Row>  
