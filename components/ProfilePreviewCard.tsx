07a089deef107cbae963088f9642a0f6b38961f9
Jack Lin <hellojjlin@gmail.com>
2023-09-26 02:25:32 -0400

    finished styling on home page

--- components/ProfilePreviewCard.tsx
+++ components/ProfilePreviewCard.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/profilePreviewCard.module.css";
 import { GetServerSideProps } from "next";
 import { DailyOpenClose } from "@/models/DailyOpenClose";
 import { useState, useEffect } from "react";
+import { Montserrat } from 'next/font/google'
 
 interface ProfilePreviewCardProps {
   name: string,
@@ -10,13 +11,15 @@ interface ProfilePreviewCardProps {
   stockData: DailyOpenClose,
 }
 
+const montserrat = Montserrat({subsets: ['latin']});
+
 // Below is not working for some reason.
 const ProfilePreviewCard = ( {name, imageURL, stockData} : ProfilePreviewCardProps ) => {
   return (
-    <Card className="h-100 border-none" bg="dark" text="white" style={{ width: '18rem' }}>
+    <Card className={`h-100 border-none ${styles.cardStyles}`} text="white">
       <Card.Img src={imageURL} variant="top" className={`img-fluid ${styles.imageStyles}`} />
       <Card.Body className="mt-2">
-        <Card.Title className="display-5">{ name }</Card.Title>
+        <Card.Title className={`display-5 ${styles.cardTitleStyles} ${montserrat.className}`}>{ name }</Card.Title>
       </Card.Body>
       <ListGroup variant="flush" className={`${styles.listGroupStyles}`}>
         <ListGroup.Item className={`lead ${styles.listItemStyle}`}>Open: <strong>${ (stockData.open)?.toFixed(2) }</strong></ListGroup.Item>
