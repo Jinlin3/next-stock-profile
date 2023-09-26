8cd2015379fe48b33ee723ecfe4e5849707b67a6
Jack Lin <hellojjlin@gmail.com>
2023-09-26 02:25:40 -0400

    finished styling on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container fluid="xxl" className={ styles.pageContainer }>
+      <Container className={ styles.pageContainer }>
         <Component {...pageProps} />
       </Container>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Open_Sans } from 'next/font/google'
+import { Open_Sans, Montserrat } from 'next/font/google'
 import { Alert, Button, Col, Container, Form, Row } from 'react-bootstrap';
 import ProfilePreviewCard from '@/components/ProfilePreviewCard';
 import { CompanyNameAndURL } from '@/models/CompanyNameAndURL';
@@ -18,16 +18,17 @@ interface HomeProps {
 }
 
 const openSans = Open_Sans({ subsets: ['latin'] });
+const montserrat = Montserrat({subsets: ['latin']});
 
 const companies: CompanyNameAndURL[] = [
   { name: 'AAPL', URL: 'https://static.vecteezy.com/system/resources/previews/017/221/833/non_2x/apple-logo-free-png.png'},
+  { name: 'MSFT', URL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png'},
+  { name: 'V', URL: 'https://cdn-icons-png.flaticon.com/512/217/217425.png'},
+  { name: 'GOOG', URL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png'},
   { name: 'AMZN', URL: 'https://s3-symbol-logo.tradingview.com/amazon--600.png'},
-  { name: 'GOOGL', URL: 'https://s3-symbol-logo.tradingview.com/alphabet--600.png'},
-  { name: 'META', URL: 'https://static.vecteezy.com/system/resources/previews/022/100/699/original/meta-logo-transparent-free-png.png'},
-  { name: 'IBM', URL: 'https://www.ibm.com/brand/experience-guides/developer/b1db1ae501d522a1a4b49613fe07c9f1/01_8-bar-positive.svg'},
-  { name: 'TSLA', URL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Tesla_logo.png/640px-Tesla_logo.png'},
-  { name: 'BABA', URL: 'https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/alibaba-512.png'},
   { name: 'NVDA', URL: 'https://cdn-icons-png.flaticon.com/512/732/732230.png'},
+  { name: 'META', URL: 'https://cdn-icons-png.flaticon.com/512/6033/6033716.png'},
+  { name: 'TSLA', URL: 'https://s3-symbol-logo.tradingview.com/tesla--600.png'},
 ];
 
 // gets current date to concat the api string
@@ -66,12 +67,12 @@ export default function Home({companyPreviewDataArray}: HomeProps) {
       </Head>
       <main className={openSans.className}>
         <div className="d-flex flex-column align-items-center pb-3">
-          <h1 className={`display-1 text-center ${styles.h1Styles}`}>TradeTide</h1>
+          <h1 className={`display-1 text-center ${styles.h1Styles} ${montserrat.className}`}>TradeTide</h1>
           <h2 className={`text-center ${styles.subHeadingStyles}`}>Simple and Accurate Data</h2>
           <h3 className="mt-2 text-center text-white">{`Yesterday: ${formattedDate}`}</h3>
         </div>
         <Alert className="text-center">
-          This page uses <strong>getServerSideProps</strong> to fetch data server side, which <strong>improves user experience and SEO.</strong>
+          This page uses <strong>getServerSideProps</strong> to fetch data server side, which allows for <strong>improved user experience and SEO.</strong>
         </Alert>
         <Alert className="text-center">
           This page runs on <strong><a href="https://polygon.io/" target="_blank">polygon.io</a></strong>.
@@ -79,15 +80,15 @@ export default function Home({companyPreviewDataArray}: HomeProps) {
         <Form className="py-3">
           <Form.Group className="mb-3 d-flex flex-column align-items-center">
             <Form.Label className="display-4 text-white fw-300">Search Individual Stocks</Form.Label>
-            <Form.Control name="searchQuery" placeholder="E.g. JNJ, WMT, TSM, ..." className="py-3" />
+            <Form.Control name="searchQuery" placeholder="E.g. JNJ, WMT, TSM, ..." className={`py-3 ${styles.searchBarStyles}`}/>
           </Form.Group>
           <div className="d-flex flex-column align-items-center">
             <Button className="btn-lg" type="submit">Search</Button>
           </div>
         </Form>
-        <h2 className="display-5 text-center my-4 text-white">Top Tech Companies</h2>
-        <div className="pb-5 justify-content-center">
-          <Row xs={1} sm={2} xl={3} className="g-3">
+        <h2 className="display-5 text-center my-4 text-white">Largest Companies</h2>
+        <div className="pb-5">
+          <Row xs={1} sm={2} xl={3} className="g-3 align-items-center">
             {companyPreviewDataArray.map((company) => (
               <Col xl={3} lg={3} md={6} xs={10} key={company.name} className="mx-auto">
                 <ProfilePreviewCard name={company.name} imageURL={company.imageURL} stockData={company.stockData} />
