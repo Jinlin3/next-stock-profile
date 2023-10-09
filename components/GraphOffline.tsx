9de8389d7072dfb330b2c101b4708b3182e2f04a
Jack Lin <hellojjlin@gmail.com>
2023-10-09 12:59:23 -0400

    removed apostrophe

--- components/GraphOffline.tsx
+++ components/GraphOffline.tsx
@@ -5,8 +5,8 @@ const GraphOffline = () => {
   return (  
     <Card className={`mt-4 ${styles.cardStyles}`}>
       <Card.Body className="d-flex flex-column align-items-center justify-content-center">
-        <h1 className="text-center text-white mt-2">{"Graph is Offline"}</h1>
-        <p className="text-center text-white lead mt-1">Due to complications with <strong>polygon.io's</strong> API, the graph cannot be displayed.</p>
+        <h1 className="text-center text-white mt-2">Graph is Offline</h1>
+        <p className="text-center text-white lead mt-1">Due to complications with <strong>polygon.io&apos;s</strong> API, the graph cannot be displayed.</p>
       </Card.Body>
     </Card>
   );
